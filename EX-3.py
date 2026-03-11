ba2fb8087b4aec3ac66d0a5f55442c7be508af22
juug24bca36105-dot <juug24bca36105@jainuniversity.ac.in>
2026-03-11 21:29:38 +0530

    various string operations

---
+++ EX-3.py
@@ -0,0 +1,31 @@
+# Aim :
+#   To write a program to implement various string operations in Python.
+
+# String operations
+s = "Hello"
+
+print("Original String:", s)
+
+print("Uppercase:", s.upper())
+print("Lowercase:", s.lower())
+
+print("Length of string:", len(s))
+
+print("Concatenation:", s + " Python")
+
+#output:
+    # Original String: Hello
+    # Uppercase: HELLO
+    # Lowercase: hello
+    # Length of string: 5
+    # Concatenation: Hello Python
+
+# Algorithm:
+# 	1.	Start the program.
+# 	2.	Declare a string variable.
+# 	3.	Display the original string.
+# 	4.	Convert the string to uppercase and lowercase.
+# 	5.	Find the length of the string.
+# 	6.	Concatenate another string.
+# 	7.	Display the results.
+# 	8.	Stop the program.
